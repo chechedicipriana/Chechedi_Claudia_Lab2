7274e54c63f170f0b4ceb3be9d8a8803a502b1f3
HP <HP@DESKTOP-JT6K0AJ>
2023-11-18 16:08:08 +0200

    Add project files.

---
+++ Data/Chechedi_Claudia_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chechedi_Claudia_Lab2.Models;
+
+namespace Chechedi_Claudia_Lab2.Data
+{
+    public class Chechedi_Claudia_Lab2Context : DbContext
+    {
+        public Chechedi_Claudia_Lab2Context (DbContextOptions<Chechedi_Claudia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chechedi_Claudia_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Chechedi_Claudia_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Chechedi_Claudia_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Chechedi_Claudia_Lab2.Data;
+using Chechedi_Claudia_Lab2.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Chechedi_Claudia_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Chechedi_Claudia_Lab2.Data.Chechedi_Claudia_Lab2Context _context;
+
+        public IndexModel(Chechedi_Claudia_Lab2.Data.Chechedi_Claudia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.Include(b => b.Publisher).Include(b => b.Author).ToListAsync();
+            }
+        }
+    }
+}
